983dd62034612a7439f8ec69d2d1a167f400e60e
Petar Borisovski <borisovski-petar@hotmail.com>
2020-10-27 21:13:38 +0100

    Add spaces

--- warden.sh
+++ warden.sh
@@ -18,14 +18,23 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 printf "Add Docker’s official GPG key:" 
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
 sudo apt-key fingerprint 0EBFCD88
+
 printf "Add Docker Repository"
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian  $(lsb_release -cs)  stable"
+
 printf "Install Docker Engine"
+
 sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 printf "Varifing Docker Engine is installed"
+
 sudo docker run hello-world
 
 brew install davidalger/warden/warden
